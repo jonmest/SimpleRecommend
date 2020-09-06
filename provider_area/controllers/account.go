45fafe878e00e6dc901a60ab6ab068efff8e3d62
J. William Cavallie Mester <jc222rz@student.lnu.se>
2020-09-06 20:06:58 +0200

    Touching up email verification template

--- provider_area/controllers/account.go
+++ provider_area/controllers/account.go
@@ -267,11 +267,11 @@ func RequestVerifyEmail(c *fiber.Ctx) {
 		return
 	}
 
-	from := mail.NewEmail("Example User", "contrarianandfree@gmail.com")
-	subject := "Sending with Twilio SendGrid is Fun"
-	to := mail.NewEmail("Example User", "jonmester3@gmail.com")
-	plainTextContent := "and easy to do anywhere, even with Go"
-	htmlContent := "<strong>and easy to do anywhere, even with Go</strong>. <a href='localhost:3001/verify-email-token/'" + tokenString + ">Click here to verify.</a>"
+	from := mail.NewEmail("SimpleRecommend", "contrarianandfree@gmail.com")
+	subject := "Verify Your Email At SimpleRecommend"
+	to := mail.NewEmail(user.Username, "jonmester3@gmail.com")
+	plainTextContent := "Thank you for signing up to SimpleRecommend. Verify your email to fully access your account now: https://app.simplerecommend.com/verify-email-token/" + tokenString
+	htmlContent := "<div>Thank you for subscribing to the SimpleRecommend service. You need to verify your email to fully access your account. <a href=\"" + tokenString + "\">Click here now to verify email</a>. If you haven't recently signed up with SimpleRecommend, disregard this email and we'll stop bothering you.</div>"
 	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
 	client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
 	response, err := client.Send(message)
